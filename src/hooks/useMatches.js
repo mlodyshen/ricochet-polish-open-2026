d8011acd1811d6b4e3e8d8d3c66a2cbea6b022b4
mlodyshen <kacpership@gmail.com>
2026-02-02 20:37:44 +0100

    DEPLOY v14: Hardcoded Zaborowska Drop (WB15 -> P17 M8) and Placement Islands

--- src/hooks/useMatches.js
+++ src/hooks/useMatches.js
@@ -4,7 +4,7 @@ import { useTournament } from '../contexts/TournamentContext';
 import { db, isFirebaseConfigured } from '../lib/firebase';
 import { collection, onSnapshot, getDocs, addDoc, updateDoc, deleteDoc, doc, query, where, writeBatch } from 'firebase/firestore';
 
-const BASE_KEY = 'brazilian_v13_FULL_PLACEMENT';
+const BASE_KEY = 'brazilian_v14_ZABOROWSKA_CORE';
 
 export const useMatches = () => {
     const [matches, setMatches] = useState([]);
